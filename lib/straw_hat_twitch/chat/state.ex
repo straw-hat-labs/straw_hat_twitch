35523dc32d0757741e1ebe1bdd183c0f457c7551
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-02-19 04:30:09 -0800

    Add chat state docs

--- lib/straw_hat_twitch/chat/state.ex
+++ lib/straw_hat_twitch/chat/state.ex
@@ -1,8 +1,30 @@
 defmodule StrawHat.Twitch.Chat.State do
+  @moduledoc """
+  It defines the state by the chat.
+  """
+
+  alias StrawHat.Twitch.Chat.Credentials
+
   @enforce_keys [:credentials, :conn_pid, :is_ready, :channels, :message_broker]
   defstruct [:credentials, :conn_pid, :is_ready, :channels, :message_broker]
 
-  def new(credentials, message_broker) do
+  @typedoc """
+  - `conn_pid`: `gun` connection PID.
+  - `is_ready`: defines if the chat is ready to interact with.
+  - `channels`: list of channels currently listen to.
+  - `message_broker`: Message Broker module used that gets notify private
+  messages.
+  """
+  @type t :: %__MODULE__{
+    conn_pid: pid() | nil,
+    credentials: %Credentials{},
+    is_ready: boolean(),
+    channels: [String.t()],
+    message_broker: module()
+  }
+
+  @doc false
+  def new(%Credentials{} = credentials, message_broker) do
     %__MODULE__{
       conn_pid: nil,
       credentials: credentials,
@@ -12,6 +34,7 @@ defmodule StrawHat.Twitch.Chat.State do
     }
   end
 
+  @doc false
   def add_channel(state, channel_name) do
     if has_channel?(state, channel_name) do
       state
@@ -21,6 +44,7 @@ defmodule StrawHat.Twitch.Chat.State do
     end
   end
 
+  @doc false
   def remove_channel(state, channel_name) do
     if has_channel?(state, channel_name) do
       channels = List.delete(state.channels, channel_name)
@@ -30,14 +54,17 @@ defmodule StrawHat.Twitch.Chat.State do
     end
   end
 
+  @doc false
   def ready(state) do
     Map.put(state, :is_ready, true)
   end
 
+  @doc false
   def save_conn_pid(state, conn_pid) do
     Map.put(state, :conn_pid, conn_pid)
   end
 
+  @doc false
   def has_channel?(state, channel_name) do
     Enum.member?(state.channels, channel_name)
   end
