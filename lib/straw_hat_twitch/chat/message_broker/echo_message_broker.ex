683421e4774e0034452b02dfbfd52724eb2de1cf
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-02-20 21:28:35 -0800

    Fix echo message broker
    - Improve docs

--- lib/straw_hat_twitch/chat/message_broker/echo_message_broker.ex
+++ lib/straw_hat_twitch/chat/message_broker/echo_message_broker.ex
@@ -1,10 +1,13 @@
 defmodule StrawHat.Twitch.Chat.EchoMessageBroker do
+  @moduledoc """
+  Echos the new messages back to the channel that receive the message.
+  """
   @behaviour StrawHat.Twitch.Chat.MessageBroker
 
   @impl StrawHat.Twitch.Chat.MessageBroker
   def publish(caller, message) do
     Task.async(fn ->
-      StrawHat.Twitch.ChatServer.send_message(caller, message.username, message.body)
+      StrawHat.Twitch.ChatServer.send_message(caller, message.channel_name, message.body)
     end)
   end
 end
