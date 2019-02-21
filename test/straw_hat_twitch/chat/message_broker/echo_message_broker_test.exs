409cee3babd0cac19cef261ab63ccec9ec6ae0a5
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-02-20 22:35:35 -0800

    Fix echo message broker tests

--- test/straw_hat_twitch/chat/message_broker/echo_message_broker_test.exs
+++ test/straw_hat_twitch/chat/message_broker/echo_message_broker_test.exs
@@ -1,12 +1,10 @@
 defmodule StrawHat.Twitch.EchoMessageBrokerTests do
   use ExUnit.Case, async: true
 
-  alias StrawHat.Twitch.ChatServer
-  alias StrawHat.Twitch.Chat.{Credentials, Message}
+  alias StrawHat.Twitch.Chat.Message
   alias StrawHat.Twitch.Chat.EchoMessageBroker
 
-
-  test "echoing the message back to the chat server", %{chat_server: _chat_server} do
+  test "echoing the message back to the chat server" do
     EchoMessageBroker.publish(self(), %Message{
       channel_name: "alchemist_ubi",
       username: "alchemist_ubi",
