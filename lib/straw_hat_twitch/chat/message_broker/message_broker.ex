51b6f03ad91cb879ec0d038d909030463340e9e8
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-02-20 21:27:37 -0800

    Add documentation for Message Broker

--- lib/straw_hat_twitch/chat/message_broker/message_broker.ex
+++ lib/straw_hat_twitch/chat/message_broker/message_broker.ex
@@ -1,3 +1,26 @@
 defmodule StrawHat.Twitch.Chat.MessageBroker do
+  @moduledoc """
+  Message Brokers are used for listening to new messages coming from Twitch chat.
+  This allow you to transform or do some async actions on the message.
+  """
+
+  @doc """
+  It receive a message from the Twitch chat. The `caller` is the `PID` of the
+  `StrawHat.Twitch.ChatServer` that received the message.
+
+  You can use the `caller` to send messages back to the subscribed chat server.
+
+  For example, the following message broker echos the message back:
+
+      defmodule EchoMessageBroker do
+        @behaviour StrawHat.Twitch.Chat.MessageBroker
+
+        @impl StrawHat.Twitch.Chat.MessageBroker
+        def publish(caller, message) do
+          # Notice how we use `caller` to send a message back to the GenServer.
+          StrawHat.Twitch.ChatServer.send_message(caller, message.channel_name, message.body)
+        end
+      end
+  """
   @callback publish(caller :: pid, message :: StrawHat.Twitch.Chat.Message.t) :: no_return
 end
